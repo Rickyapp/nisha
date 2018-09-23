dafdf707fc510fdc675a0bdb14d154b537396982
Rickyapp <ricky.it.hack@gmail.com>
2018-09-23 13:25:12 +0530

    about php file

---
+++ about.php
@@ -0,0 +1,232 @@
+<!doctype html>
+<html>
+
+<!-- Mirrored from crunchpress.com/html/iknow/about-us.html by HTTrack Website Copier/3.x [XR&CO'2014], Tue, 29 Mar 2016 17:58:08 GMT -->
+<head>
+<meta charset="utf-8">
+<title>Education</title>
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+ 
+<link href="css/style.css" rel="stylesheet" media="screen">
+<link href="css/color.css" rel="stylesheet" media="screen">
+<link href="css/transitions.css" rel="stylesheet" media="screen">
+ 
+<link href="css/bootstrap.css" rel="stylesheet" media="screen">
+<link href="css/bootstrap-responsive.css" rel="stylesheet" media="screen">
+ 
+<link href="css/jquery.bxslider.css" rel="stylesheet" media="screen">
+ 
+<link href="css/owl.carousel.css" rel="stylesheet">
+ 
+<link href="css/font-awesome.min.css" rel="stylesheet" media="screen">
+ 
+<link href="css/parallax.css" rel="stylesheet" type="text/css">
+ 
+<link href="css/chart.css" rel="stylesheet" type="text/css">
+</head>
+<body>
+ 
+<div class="wrapper">
+ 
+<header>
+ 
+<div class="top-strip">
+<div class="container">
+ 
+<div class="language">
+<div class="dropdown">
+<a class="dropdown-toggle" id="dLabel1" role="button" data-toggle="dropdown" data-target="#" href="http://crunchpress.com/page.html">
+Language
+<b class="caret"></b>
+</a>
+<ul class="dropdown-menu" role="menu" aria-labelledby="dLabel1">
+<li><a href="#">English</a></li>
+<li><a href="#">Spenish</a></li>
+<li><a href="#">Turkish</a></li>
+<li><a href="#">Arabic</a></li>
+</ul>
+</div>
+<div class="dropdown">
+<a class="dropdown-toggle" id="currency" role="button" data-toggle="dropdown" data-target="#" href="http://crunchpress.com/page.html">
+Currency
+<b class="caret"></b>
+</a>
+<ul class="dropdown-menu" role="menu" aria-labelledby="currency">
+<li><a href="#">Dollar</a></li>
+<li><a href="#">Euro</a></li>
+<li><a href="#">Dirham</a></li>
+<li><a href="#">Rupees</a></li>
+<li><a href="#">Dinar</a></li>
+</ul>
+</div>
+</div>
+ 
+ 
+<div class="account">
+<ul>
+<li><a href="#">Subscribe</a></li>
+<li><a href="#">Sign Out</a></li>
+<li>
+<div class="dropdown">
+<a class="dropdown-toggle" id="account" role="button" data-toggle="dropdown" data-target="#" href="http://crunchpress.com/page.html">
+My Account
+<b class="caret"></b>
+</a>
+<ul class="dropdown-menu" role="menu" aria-labelledby="account">
+<li><a href="#">Profile</a></li>
+<li><a href="#">Account Setting</a></li>
+<li><a href="#">Privacy Setting</a></li>
+</ul>
+</div>
+</li>
+</ul>
+</div>
+ 
+</div>
+</div>
+ 
+ 
+<?php include "Header.php";?>
+ 
+<div class="contant">
+ 
+<section class="gray-bg">
+<div class="container">
+<div class="row">
+<div class="span6">
+<div class="intro">
+<h2>Everything you need to know</h2>
+<p>Gestated to serve a blend of luxury and comfort while enjoying Indian beauty, Taj Mahal Same Day Tour is a mix of relaxing tour, innovative package deals and impeccable hospitality. Serving as one-stop-shop for all your travel needs, the company has revolutionized the functionality of travel industry and made it more prominent towards creating ways of a smooth journey. From customized tours to fixed packages, ticket booking, hotel reservation and various other facilities, Taj Mahal Same Day Tour has enabled travelers to enjoy their vacations completely. With all the positive feedbacks this online travel operator has made a favored name in the travel service providing company.</p>
+</div>
+</div>
+<div class="span6">
+<div class="mac">
+<ul class="bxslider">
+<li>
+<img src="images/mac-screen1.png" alt="">
+</li>
+<li>
+<img src="images/mac-screen2.png" alt="">
+</li>
+</ul>
+</div>
+</div>
+</div>
+</div>
+</section>
+ 
+ 
+<section>
+<div class="container">
+ 
+<div class="sec-header">
+<h2>Our Teachers</h2>
+<p>Discover courses by topic</p>
+<span></span>
+<span></span>
+<span></span>
+</div>
+ 
+<div class="row">
+ 
+<div class="span3">
+<div class="teachers">
+<div class="social-icons">
+<a href="#"><i class="fa fa-facebook"></i></a>
+<a href="#"><i class="fa fa-flickr"></i></a>
+<a href="#"><i class="fa fa-dribbble"></i></a>
+<a href="#"><i class="fa fa-google-plus"></i></a>
+<a href="#"><i class="fa fa-linkedin"></i></a>
+<a href="#"><i class="fa fa-pinterest"></i></a>
+<a href="#"><i class="fa fa-twitter"></i></a>
+<a href="#"><i class="fa fa-vimeo-square"></i></a>
+</div>
+<div class="thumb">
+<a href="#"><img src="images/teachers1.jpg" alt=""></a>
+</div>
+<div class="text">
+<h2><a href="#">Mickey Rorey</a></h2>
+<p>Science Teacher</p>
+</div>
+</div>
+</div>
+ 
+ 
+<div class="span3">
+<div class="teachers">
+<div class="social-icons">
+<a href="#"><i class="fa fa-facebook"></i></a>
+<a href="#"><i class="fa fa-flickr"></i></a>
+<a href="#"><i class="fa fa-dribbble"></i></a>
+<a href="#"><i class="fa fa-google-plus"></i></a>
+<a href="#"><i class="fa fa-linkedin"></i></a>
+<a href="#"><i class="fa fa-pinterest"></i></a>
+<a href="#"><i class="fa fa-twitter"></i></a>
+<a href="#"><i class="fa fa-vimeo-square"></i></a>
+</div>
+<div class="thumb">
+<a href="#"><img src="images/teachers2.jpg" alt=""></a>
+</div>
+<div class="text">
+<h2><a href="#">Mickey Rorey</a></h2>
+<p>Science Teacher</p>
+</div>
+</div>
+</div>
+ 
+ 
+<div class="span3">
+<div class="teachers">
+<div class="social-icons">
+<a href="#"><i class="fa fa-facebook"></i></a>
+<a href="#"><i class="fa fa-flickr"></i></a>
+<a href="#"><i class="fa fa-dribbble"></i></a>
+<a href="#"><i class="fa fa-google-plus"></i></a>
+<a href="#"><i class="fa fa-linkedin"></i></a>
+<a href="#"><i class="fa fa-pinterest"></i></a>
+<a href="#"><i class="fa fa-twitter"></i></a>
+<a href="#"><i class="fa fa-vimeo-square"></i></a>
+</div>
+<div class="thumb">
+<a href="#"><img src="images/teachers3.jpg" alt=""></a>
+</div>
+<div class="text">
+<h2><a href="#">Mickey Rorey</a></h2>
+<p>Science Teacher</p>
+</div>
+</div>
+</div>
+ 
+ 
+<div class="span3">
+<div class="teachers">
+<div class="social-icons">
+<a href="#"><i class="fa fa-facebook"></i></a>
+<a href="#"><i class="fa fa-flickr"></i></a>
+<a href="#"><i class="fa fa-dribbble"></i></a>
+<a href="#"><i class="fa fa-google-plus"></i></a>
+<a href="#"><i class="fa fa-linkedin"></i></a>
+<a href="#"><i class="fa fa-pinterest"></i></a>
+<a href="#"><i class="fa fa-twitter"></i></a>
+<a href="#"><i class="fa fa-vimeo-square"></i></a>
+</div>
+<div class="thumb">
+<a href="#"><img src="images/teachers4.jpg" alt=""></a>
+</div>
+<div class="text">
+<h2><a href="#">Mickey Rorey</a></h2>
+<p>Science Teacher</p>
+</div>
+</div>
+</div>
+ 
+</div>
+</div>
+</section>
+ 
+
+
+ 
+</div>
+ 
+<?php include"footer.php";?>
